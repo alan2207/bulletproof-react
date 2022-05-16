cd0b86b4f08409abc633c9408aa64037f4434793
Nguyen Cao Cuong - Peter James <50004497+caocuong2404@users.noreply.github.com>
2022-05-16 11:52:35 +0700

    fix: the`initMocks` function always starts
    The value of `.env.*` files variables always string value. 
    - ref: https://github.com/motdotla/dotenv/blob/master/README.md#what-rules-does-the-parsing-engine-follow

--- src/test/server/index.ts
+++ src/test/server/index.ts
@@ -1,5 +1,5 @@
 export const initMocks = () => {
-  if (!!process.env.REACT_APP_API_MOCKING === true) {
+  if (process.env.REACT_APP_API_MOCKING === 'true') {
     if (typeof window === 'undefined') {
       // eslint-disable-next-line @typescript-eslint/no-var-requires
       const { server } = require('./server');
