0b6ea8af69b06f02d9f28deb78e6ff3ea73c1f95
ryuji-1to <ryuji0222ss@icloud.com>
2023-04-12 20:45:04 +0900

    improve: 🎨 update form

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,17 +1,16 @@
 import { zodResolver } from '@hookform/resolvers/zod';
 import clsx from 'clsx';
+import type { ComponentPropsWithoutRef } from 'react';
 import * as React from 'react';
 import { useForm, UseFormReturn, SubmitHandler, UseFormProps } from 'react-hook-form';
 import { ZodType, ZodTypeDef } from 'zod';
 
 type FormProps<TFormValues, Schema> = {
-  className?: string;
   onSubmit: SubmitHandler<TFormValues>;
   children: (methods: UseFormReturn<TFormValues>) => React.ReactNode;
   options?: UseFormProps<TFormValues>;
-  id?: string;
   schema?: Schema;
-};
+} & Omit<ComponentPropsWithoutRef<'form'>, 'onSubmit'>;
 
 export const Form = <
   TFormValues extends Record<string, unknown> = Record<string, unknown>,
@@ -21,15 +20,15 @@ export const Form = <
   children,
   className,
   options,
-  id,
   schema,
+  ...rest
 }: FormProps<TFormValues, Schema>) => {
   const methods = useForm<TFormValues>({ ...options, resolver: schema && zodResolver(schema) });
   return (
     <form
+      {...rest}
       className={clsx('space-y-6', className)}
       onSubmit={methods.handleSubmit(onSubmit)}
-      id={id}
     >
       {children(methods)}
     </form>
