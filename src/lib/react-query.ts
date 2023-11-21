13822432f49deca984ab1cc53c5dea97a3ad00c7
StephenODea54 <odeastephen1@gmail.com>
2023-11-21 18:58:42 -0500

    Remove PromiseValue for Awaited

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -1,6 +1,5 @@
 import { AxiosError } from 'axios';
 import { QueryClient, UseQueryOptions, UseMutationOptions, DefaultOptions } from 'react-query';
-import { PromiseValue } from 'type-fest';
 
 const queryConfig: DefaultOptions = {
   queries: {
@@ -12,9 +11,7 @@ const queryConfig: DefaultOptions = {
 
 export const queryClient = new QueryClient({ defaultOptions: queryConfig });
 
-export type ExtractFnReturnType<FnType extends (...args: any) => any> = PromiseValue<
-  ReturnType<FnType>
->;
+export type ExtractFnReturnType<FnType extends (...args: any) => any> = Awaited<ReturnType<FnType>>;
 
 export type QueryConfig<QueryFnType extends (...args: any) => any> = Omit<
   UseQueryOptions<ExtractFnReturnType<QueryFnType>>,
