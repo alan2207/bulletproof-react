76065e9630c280e599fb0d05451bc10cc242d27a
Alan Alickovic <alan2207@live.com>
2024-05-11 08:35:39 +0200

    refactor playwright tests

--- e2e/tests/smoke.spec.ts
+++ e2e/tests/smoke.spec.ts
@@ -1,57 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-import { createUser, createDiscussion, createComment } from '../../src/testing/data-generators';
+import {
+  createDiscussion,
+  createComment,
+} from '../../src/testing/data-generators';
 test('smoke', async ({ page }) => {
-  const user = createUser();
-  const discussion = createDiscussion({
-    authorId: user.id,
-  });
-  const comment = createComment({
-    authorId: user.id,
-  });
+  const discussion = createDiscussion();
+  const comment = createComment();
 
   await page.goto('/');
-  await expect(page.getByRole('heading', { name: 'Bulletproof React' })).toBeVisible();
   await page.getByRole('button', { name: 'Get started' }).click();
-
-  await page.waitForURL('/auth/login');
-
-  // registration:
-  await page.getByRole('link', { name: 'Register' }).click();
-
-  await page.waitForURL('/auth/register');
-  await page.getByLabel('First Name').click();
-  await page.getByLabel('First Name').fill(user.firstName);
-  await page.getByLabel('Last Name').click();
-  await page.getByLabel('Last Name').fill(user.lastName);
-  await page.getByLabel('Email Address').click();
-  await page.getByLabel('Email Address').fill(user.email);
-  await page.getByLabel('Password').click();
-  await page.getByLabel('Password').fill(user.password);
-  await page.getByLabel('Team Name').click();
-  await page.getByLabel('Team Name').fill(user.teamName);
-  await page.getByRole('button', { name: 'Register' }).click();
-
   await page.waitForURL('/app');
-  await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
-  await expect(
-    page.getByRole('heading', { name: `Welcome ${user.firstName} ${user.lastName}` })
-  ).toBeVisible();
-
-  // log out:
-  await page.getByRole('button', { name: 'Open user menu' }).click();
-  await page.getByRole('menuitem', { name: 'Sign Out' }).click();
-  await page.waitForURL('/auth/login?redirectTo=%2Fapp');
-
-  // log in:
-  await page.getByLabel('Email Address').click();
-  await page.getByLabel('Email Address').fill(user.email);
-  await page.getByLabel('Password').click();
-  await page.getByLabel('Password').fill(user.password);
-  await page.getByRole('button', { name: 'Log in' }).click();
-  await expect(
-    page.getByRole('heading', { name: `Welcome ${user.firstName} ${user.lastName}` })
-  ).toBeVisible();
 
   // create discussion:
   await page.getByRole('link', { name: 'Discussions' }).click();
@@ -63,12 +22,17 @@ test('smoke', async ({ page }) => {
   await page.getByLabel('Body').click();
   await page.getByLabel('Body').fill(discussion.body);
   await page.getByRole('button', { name: 'Submit' }).click();
-  await page.getByLabel('Discussion Created').getByRole('button', { name: 'Close' }).click();
+  await page
+    .getByLabel('Discussion Created')
+    .getByRole('button', { name: 'Close' })
+    .click();
 
   // visit discussion page:
   await page.getByRole('link', { name: 'View' }).click();
 
-  await expect(page.getByRole('heading', { name: discussion.title })).toBeVisible();
+  await expect(
+    page.getByRole('heading', { name: discussion.title }),
+  ).toBeVisible();
   await expect(page.getByText(discussion.body)).toBeVisible();
 
   // update discussion:
@@ -78,9 +42,14 @@ test('smoke', async ({ page }) => {
   await page.getByLabel('Body').click();
   await page.getByLabel('Body').fill(`${discussion.body} - updated`);
   await page.getByRole('button', { name: 'Submit' }).click();
-  await page.getByLabel('Discussion Updated').getByRole('button', { name: 'Close' }).click();
+  await page
+    .getByLabel('Discussion Updated')
+    .getByRole('button', { name: 'Close' })
+    .click();
 
-  await expect(page.getByRole('heading', { name: `${discussion.title} - updated` })).toBeVisible();
+  await expect(
+    page.getByRole('heading', { name: `${discussion.title} - updated` }),
+  ).toBeVisible();
   await expect(page.getByText(`${discussion.body} - updated`)).toBeVisible();
 
   // create comment:
@@ -89,14 +58,24 @@ test('smoke', async ({ page }) => {
   await page.getByLabel('Body').fill(comment.body);
   await page.getByRole('button', { name: 'Submit' }).click();
   await expect(page.getByText(comment.body)).toBeVisible();
-  await page.getByLabel('Comment Created').getByRole('button', { name: 'Close' }).click();
+  await page
+    .getByLabel('Comment Created')
+    .getByRole('button', { name: 'Close' })
+    .click();
 
   // delete comment:
   await page.getByRole('button', { name: 'Delete Comment' }).click();
-  await expect(page.getByText('Are you sure you want to delete this comment?')).toBeVisible();
+  await expect(
+    page.getByText('Are you sure you want to delete this comment?'),
+  ).toBeVisible();
   await page.getByRole('button', { name: 'Delete Comment' }).click();
-  await page.getByLabel('Comment Deleted').getByRole('button', { name: 'Close' }).click();
-  await expect(page.getByRole('heading', { name: 'No Comments Found' })).toBeVisible();
+  await page
+    .getByLabel('Comment Deleted')
+    .getByRole('button', { name: 'Close' })
+    .click();
+  await expect(
+    page.getByRole('heading', { name: 'No Comments Found' }),
+  ).toBeVisible();
   await expect(page.getByText(comment.body)).toBeHidden();
 
   // go back to discussions:
@@ -106,16 +85,11 @@ test('smoke', async ({ page }) => {
   // delete discussion:
   await page.getByRole('button', { name: 'Delete Discussion' }).click();
   await page.getByRole('button', { name: 'Delete Discussion' }).click();
-  await page.getByLabel('Discussion Deleted').getByRole('button', { name: 'Close' }).click();
-  await expect(page.getByRole('heading', { name: 'No Entries Found' })).toBeVisible();
-
-  // update user:
-  await page.getByRole('button', { name: 'Open user menu' }).click();
-  await page.getByRole('menuitem', { name: 'Your Profile' }).click();
-  await page.getByRole('button', { name: 'Update Profile' }).click();
-  await page.getByLabel('Bio').click();
-  await page.getByLabel('Bio').fill('My bio');
-  await page.getByRole('button', { name: 'Submit' }).click();
-  await page.getByLabel('User Updated').getByRole('button', { name: 'Close' }).click();
-  await expect(page.getByText('My bio')).toBeVisible();
+  await page
+    .getByLabel('Discussion Deleted')
+    .getByRole('button', { name: 'Close' })
+    .click();
+  await expect(
+    page.getByRole('heading', { name: 'No Entries Found' }),
+  ).toBeVisible();
 });
--- playwright.config.ts
+++ playwright.config.ts
@@ -34,9 +34,15 @@ export default defineConfig({
 
   /* Configure projects for major browsers */
   projects: [
+    { name: 'setup', testMatch: /.*\.setup\.ts/ },
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+        storageState: 'e2e/.auth/user.json',
+      },
+
+      dependencies: ['setup'],
     },
   ],
 
