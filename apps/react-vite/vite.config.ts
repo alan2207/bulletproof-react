e2ffecb780ed830e817d2139a2c3e7d64ef807cc
Alan Alickovic <alan2207@live.com>
2024-08-11 11:27:19 +0200

    add experimentalMinChunkSize config to reduce amount of very small chunks

--- apps/react-vite/vite.config.ts
+++ apps/react-vite/vite.config.ts
@@ -27,6 +27,9 @@ export default defineConfig({
   build: {
     rollupOptions: {
       external: ['fs/promises'],
+      output: {
+        experimentalMinChunkSize: 3500,
+      },
     },
   },
 });
