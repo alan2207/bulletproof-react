71c1835b273ffc54cb728ba2d52c6de6845077cd
Alan Alickovic <alan2207@live.com>
2024-08-11 13:35:43 +0200

    prefetch comments

--- apps/react-vite/src/app/routes/app/discussions/discussions.tsx
+++ apps/react-vite/src/app/routes/app/discussions/discussions.tsx
@@ -1,6 +1,7 @@
-import { QueryClient } from '@tanstack/react-query';
+import { QueryClient, useQueryClient } from '@tanstack/react-query';
 
 import { ContentLayout } from '@/components/layouts';
+import { getCommentsQueryOptions } from '@/features/comments/api/get-comments';
 import { getDiscussionsQueryOptions } from '@/features/discussions/api/get-discussions';
 import { CreateDiscussion } from '@/features/discussions/components/create-discussion';
 import { DiscussionsList } from '@/features/discussions/components/discussions-list';
@@ -15,13 +16,19 @@ export const discussionsLoader = (queryClient: QueryClient) => async () => {
 };
 
 export const DiscussionsRoute = () => {
+  const queryClient = useQueryClient();
   return (
     <ContentLayout title="Discussions">
       <div className="flex justify-end">
         <CreateDiscussion />
       </div>
       <div className="mt-4">
-        <DiscussionsList />
+        <DiscussionsList
+          onDiscussionPrefetch={(id) => {
+            // Prefetch the comments data when the user hovers over the link in the list
+            queryClient.prefetchQuery(getCommentsQueryOptions(id));
+          }}
+        />
       </div>
     </ContentLayout>
   );
