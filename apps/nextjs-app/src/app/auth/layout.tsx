82695e0fe092635bc4444757875ffddd5f2b84c9
Alan Alickovic <alan2207@live.com>
2024-09-03 07:46:04 +0200

    cleanup

--- apps/nextjs-app/src/app/auth/layout.tsx
+++ apps/nextjs-app/src/app/auth/layout.tsx
@@ -2,12 +2,9 @@ import { ReactNode } from 'react';
 
 import { AuthLayout as AuthLayoutComponent } from '@/components/layouts/auth-layout';
 
-export const generateMetadata = (...args: any[]) => {
-  console.log(args);
-  return {
-    title: 'Bulletproof React',
-    description: 'Welcome to Bulletproof React',
-  };
+export const metadata = {
+  title: 'Bulletproof React',
+  description: 'Welcome to Bulletproof React',
 };
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
