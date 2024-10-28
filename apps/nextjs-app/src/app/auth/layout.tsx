ae90720c61406d2b44c541deb285d9b8ce331fad
Alan Alickovic <alan2207@live.com>
2024-10-28 05:53:46 +0100

    fix build error

--- apps/nextjs-app/src/app/auth/layout.tsx
+++ apps/nextjs-app/src/app/auth/layout.tsx
@@ -1,6 +1,8 @@
-import { ReactNode } from 'react';
+import { ReactNode, Suspense } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 
 import { AuthLayout as AuthLayoutComponent } from '@/components/layouts/auth-layout';
+import { Spinner } from '@/components/ui/spinner';
 
 export const metadata = {
   title: 'Bulletproof React',
@@ -8,7 +10,19 @@ export const metadata = {
 };
 
 const AuthLayout = ({ children }: { children: ReactNode }) => {
-  return <AuthLayoutComponent>{children}</AuthLayoutComponent>;
+  return (
+    <Suspense
+      fallback={
+        <div className="flex size-full items-center justify-center">
+          <Spinner size="xl" />
+        </div>
+      }
+    >
+      <ErrorBoundary fallback={<div>Something went wrong!</div>}>
+        <AuthLayoutComponent>{children}</AuthLayoutComponent>
+      </ErrorBoundary>
+    </Suspense>
+  );
 };
 
 export default AuthLayout;
